fe56daacfe1a80f1dcaefc6223c29c1389fa87b4
m1911star <lhlxtl@gmail.com>
2024-09-01 23:30:47 +0800

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,24 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  // add favicon
   return (
     <html lang="en">
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/site.webmanifest" />
       <body className={`h-screen w-screen`}>{children}</body>
     </html>
   );
